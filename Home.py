031d2b5c1148f045ba1aa1efb682245d3e3771a8
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-05 21:13:01 -0300

    Add logo

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import streamlit as st
+from PIL import Image
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "LAF Solutions"
@@ -15,12 +16,15 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
+logo_pic = current_dir / "assets" / "Logo e nome lateral - escuro.png"
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-st.title(NAME)
+logo = Image.open(logo_pic)
+st.image(logo, width=700)
+
 st.write(DESCRIPTION)
 
 with st.sidebar:
