f4b7c5dba6ecc30ebfa227fa283de12b6f30e6f3
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-14 21:03:41 -0300

    Change compress image function

--- pages/3_Computer_Vision.py
+++ pages/3_Computer_Vision.py
@@ -6,53 +6,41 @@ import requests
 from PIL import Image
 import io
 
-def calculate_image_size_in_kb(height, width, color_depth):
-    # Calculate the size in bytes
-    size_in_bytes = height * width * (color_depth // 8)  # Divide by 8 to convert bits to bytes
-    # Convert bytes to kilobytes
-    size_in_kb = size_in_bytes / 1024.0  # 1 KB = 1024 bytes
-    return size_in_kb
-
-def compress_image(image, max_size_kb=200):
-    # Open the uploaded image with PIL
+def compress_image(uploaded_file, max_size_kb=200):
+    """
+    Compress an image to a maximum size in kilobytes by resizing.
+    """
+    # Open the uploaded image
     image = Image.open(uploaded_file)
     img_format = image.format.lower()
 
-    # Get original image
-    compressed_image = image.resize((int(image.width), int(image.height)))
+    # Convert the maximum size to bytes
+    max_size_bytes = max_size_kb * 1024
 
-    color_mode = compressed_image.mode
-    # Extract the color depth from the color mode
-    if color_mode == "1":
-        color_depth = 1  # 1-bit image (binary)
-    elif color_mode in ["L", "P"]:
-        color_depth = 8  # 8-bit image (grayscale or palettized)
-    elif color_mode == "RGB":
-        color_depth = 24  # 8 bits per channel (Red, Green, Blue)
-    elif color_mode == "RGBA":
-        color_depth = 32  # 8 bits per channel (Red, Green, Blue, Alpha)
-    elif color_mode == "CMYK":
-        color_depth = 32  # 8 bits per channel (Cyan, Magenta, Yellow, Key/Black)
-    else:
-        # Handle other color modes as needed
-        color_depth = None
+    # Get the current image size in bytes
+    original_image_bytes = io.BytesIO()
+    image.save(original_image_bytes, format=img_format)
+    original_image_size_bytes = original_image_bytes.tell()
 
-    size_in_kb = calculate_image_size_in_kb(compressed_image.size[0], compressed_image.size[1], color_depth)
-    st.write(f"Image original size is {size_in_kb:.2f} KB")
-    while size_in_kb > max_size_kb:
-        # Compress the image by resizing it to a smaller size
-        compressed_image = compressed_image.resize((int(compressed_image.width * 0.9), int(compressed_image.height * 0.9)))
-        size_in_kb = calculate_image_size_in_kb(compressed_image.size[0], compressed_image.size[1], color_depth)
+    # Calculate the scale factor to fit within the desired size
+    scale_factor = (max_size_bytes / original_image_size_bytes) ** 0.5
 
-    st.write(f"Image compressed to {size_in_kb:.2f} KB")
-    # Convert the compressed image back to bytes
+    # Resize the image with the calculated scale factor
+    width, height = image.size
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+    resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+    # Create an in-memory buffer to store the resized image
     compressed_image_bytes = io.BytesIO()
-    compressed_image.save(compressed_image_bytes, format=img_format)
-    
-    # Reset the file pointer to the beginning of the buffer
-    compressed_image_bytes.seek(0)
 
+    # Save the resized image to the buffer
+    resized_image.save(compressed_image_bytes, format=img_format)
+
+    # Reset the buffer and return the compressed image bytes
+    compressed_image_bytes.seek(0)
     return compressed_image_bytes
+
 
 # --- REQUEST SETTINGS ---
 URL = "https://api.ocr-pytesseract.lafsolutions.com.br/ocr/"
@@ -91,18 +79,25 @@ uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg"])
 selected_language = st.selectbox("Select a language", ["eng", "por"])
 
 if uploaded_file is not None:
-    #with st.spinner("Compressing file..."):
-        #compressed_image_bytes = compress_image(uploaded_file)
+    with st.spinner("Compressing image..."):
+        file_data = uploaded_file.read()
+        original_size_kb = len(file_data)/1024
+        st.write(f"Image original size is {original_size_kb:.1f} kbs")
+        max_size_kb = 200
+        if original_size_kb > max_size_kb:
+            compressed_image_bytes = compress_image(uploaded_file, max_size_kb=max_size_kb)
+            file_data = compressed_image_bytes.read()
+        compressed_size_kb = len(file_data)/1024
+        st.write(f"Image compressed size is {compressed_size_kb:.1f} kbs")
+        #st.image(file_data)
 
     with st.spinner("Loading ocr..."):
         request_params = {"output_type": "string", "lang": selected_language, "config": "--psm 6", "nice": 0, "timeout": 0}
 
         files = []
         filename = uploaded_file.name
-        #file_data = compressed_image_bytes.read()
-        file_data = uploaded_file.read()
         files.append(('files', (filename, file_data)))
-
+        
         request = requests.post(url=URL, params=request_params, files=files) 
         data_request = request.json()
         ocr = data_request['results'][filename]
