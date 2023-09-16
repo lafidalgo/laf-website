6f93a139656f82bb03365bcfe5463389013bb3a7
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-16 14:45:55 -0300

    Update url and remove crop image

--- pages/3_OCR_Layout_Parser.py
+++ pages/3_OCR_Layout_Parser.py
@@ -6,10 +6,9 @@ import requests
 from PIL import Image, ImageEnhance
 import io
 import base64
-from streamlit_cropperjs import st_cropperjs
 
 # --- REQUEST SETTINGS ---
-URL = "https://api.ocr-pytesseract.lafsolutions.com.br/ocr/"
+URL = "https://api.ocr-layoutparser.lafsolutions.com.br/ocr/"
 # URL = "http://127.0.0.1:8000/ocr/"
 
 # --- GENERAL SETTINGS ---
@@ -39,27 +38,26 @@ uploaded_file = st.file_uploader("Upload a picture", key="uploaded_pic", type=["
 # Add a select-from-options widget
 selected_language = st.selectbox("Select a language", ["eng", "por"])
 if uploaded_file:
-    pic = uploaded_file.read()
-    cropped_pic = st_cropperjs(pic=pic, btn_text="Detect!", key="foo")
-    if cropped_pic:
-        st.image(cropped_pic, output_format="PNG")
-        file_data = cropped_pic
-        with st.spinner("Loading ocr..."):
-            request_params = {"output_type": "string", "lang": selected_language, "config": "--psm 6", "nice": 0, "timeout": 0}
+    file_data = uploaded_file.read()
 
-            files = []
-            filename = uploaded_file.name
-            files.append(('files', (filename, file_data)))
-            
-            request = requests.post(url=URL, params=request_params, files=files) 
-            data_request = request.json()
-            ocr = data_request['results'][filename]['ocr']
+    st.image(file_data, output_format="PNG")
 
-            st.success(f"{ocr}")
+    with st.spinner("Loading ocr..."):
+        request_params = {"output_type": "string", "lang": selected_language, "config": "--psm 6", "nice": 0, "timeout": 0}
 
-            final_image_base64 = data_request['results'][filename]['final_image_base64']
-            final_image_base64 = Image.open(io.BytesIO(base64.b64decode(final_image_base64)))
-            st.image(final_image_base64)
+        files = []
+        filename = uploaded_file.name
+        files.append(('files', (filename, file_data)))
+        
+        request = requests.post(url=URL, params=request_params, files=files) 
+        data_request = request.json()
+        ocr = data_request['results'][filename]['ocr']
+
+        st.success(f"{ocr}")
+
+        final_image_base64 = data_request['results'][filename]['final_image_base64']
+        final_image_base64 = Image.open(io.BytesIO(base64.b64decode(final_image_base64)))
+        st.image(final_image_base64)
 
 with st.sidebar:
     pass
