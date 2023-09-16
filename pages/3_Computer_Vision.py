dfb804747a7b2fd158af5984cb71a80b771aa8d2
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-16 14:03:12 -0300

    Remove compress_image function

--- pages/3_Computer_Vision.py
+++ pages/3_Computer_Vision.py
@@ -8,45 +8,6 @@ import io
 import base64
 from streamlit_cropperjs import st_cropperjs
 
-def compress_image(uploaded_file, max_size_kb=200):
-    """
-    Compress an image to a maximum size in kilobytes by resizing.
-    """
-    # Open the uploaded image
-    image = Image.open(uploaded_file)
-    img_format = image.format.lower()
-
-    # Convert the image to grayscale (black and white)
-    image = image.convert('L')
-
-    # Convert the maximum size to bytes
-    max_size_bytes = max_size_kb * 1024
-
-    # Get the current image size in bytes
-    original_image_bytes = io.BytesIO()
-    image.save(original_image_bytes, optimize=True, format=img_format)
-    original_image_size_bytes = original_image_bytes.tell()
-
-    # Calculate the scale factor to fit within the desired size
-    scale_factor = (max_size_bytes / original_image_size_bytes) ** 0.5
-
-    # Resize the image with the calculated scale factor
-    width, height = image.size
-    new_width = int(width * scale_factor)
-    new_height = int(height * scale_factor)
-    resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
-
-    # Create an in-memory buffer to store the resized image
-    compressed_image_bytes = io.BytesIO()
-
-    # Save the resized image to the buffer
-    resized_image.save(compressed_image_bytes, optimize=True, format=img_format)
-
-    # Reset the buffer and return the compressed image bytes
-    compressed_image_bytes.seek(0)
-    return compressed_image_bytes
-
-
 # --- REQUEST SETTINGS ---
 URL = "https://api.ocr-pytesseract.lafsolutions.com.br/ocr/"
 # URL = "http://127.0.0.1:8000/ocr/"
